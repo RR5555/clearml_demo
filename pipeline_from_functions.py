ee5bb3805e2220f2691c307919285054f782d389
blissfox-blue <renardromain@hotmail.fr>
2024-05-17 01:47:20 +0900

    Fix PipeController

--- pipeline_from_functions.py
+++ pipeline_from_functions.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
         name='Pipeline from fcts demo',
         version='1.1',
         add_pipeline_tags=False,
-        docker="rr5555/ubuntu:jammy",
+        docker="ubuntu:jammy",
         repo="https://github.com/RR5555/clearml_demo.git",
         packages="./requirements.txt"
     )
