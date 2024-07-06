15fdc52c17f4932fc6f638e29e279b8bf811e5d6
RR5555 <renardromain@hotmail.fr>
2024-07-06 19:17:11 +0900

    Update 2024/06/29 19:50:30

--- reuse_artifact.py
+++ reuse_artifact.py
@@ -7,4 +7,9 @@ fetched_task_list = Task.get_tasks(project_name='Demo', task_name='Storage', tas
 for _task in fetched_task_list:
     print(_task.artifacts['dummy_log'])
 
+with open(fetched_task.artifacts['dummy_log']['url'],'r') as _file:
+    print(_file.readlines())
+
+
+
 # local_json = fetched_task.artifacts['dummy_log'].get_local_copy()
