978fc9f53f1aba8d4d6565651ad4ca02e152ad99
RR5555 <renardromain@hotmail.fr>
2024-07-06 19:05:42 +0900

    Update 2024/06/29 19:50:30

--- reuse_artifact.py
+++ reuse_artifact.py
@@ -3,4 +3,7 @@ from clearml import Task
 
 fetched_task = Task.get_task(project_name='Demo', task_name='Storage')
 print(fetched_task.artifacts['dummy_log'])
+fetched_task_list = Task.get_tasks(project_name='Demo', task_name='Storage')
+print(fetched_task_list)
+
 # local_json = fetched_task.artifacts['dummy_log'].get_local_copy()
